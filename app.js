620c6b450933d8bc83621a14eb043b9a49df3163
vip7009pro <hung1893@gmail.com>
2022-07-05 07:48:34 +0700

    thay port sang 3007

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ io.on('connection', client => {
         console.log("Connected clients: " + io.engine.clientsCount);
     });
 });
-const port = 80;
+const port = 3007;
 var corsOptions = {
     origin: ['http://14.160.33.94:3000','http://14.160.33.94:3010','http://14.160.33.94:3030','http://localhost','https://script.google.com/'],
     optionsSuccessStatus: 200,
