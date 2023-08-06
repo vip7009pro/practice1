e6873fb22e9862e9fba74c6499eb34fbfb6a7da9
vip7009pro <hung1893@gmail.com>
2023-08-07 05:32:23 +0700

    sua lại theo web github

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-var cookieParser = require('cookie-parser');
-var api_module = require('./api');
-var cors = require('cors')
-require('dotenv').config();
-var compression = require('compression')
+var cookieParser = require("cookie-parser");
+var api_module = require("./api");
+var cors = require("cors");
+require("dotenv").config();
+var compression = require("compression");
 const fileupload = require("express-fileupload");
-var multer = require('multer');
-const  fs = require('fs');
+var multer = require("multer");
+const fs = require("fs");
 //var upload = multer({ dest: 'uploadfiles/' });
 let client_array = [];
 
@@ -43,15 +43,14 @@ else
 }
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploadfiles')
-    },
-    filename: function (req, file, cb) {       
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-            cb(null, file.originalname);
-    }
-  })
-  
+  destination: function (req, file, cb) {
+    cb(null, "uploadfiles");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
 const upload = multer({ storage: storage });
 const storage2 = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -71,14 +70,14 @@ app.use(
 );
 console.log("usser  =" + process.env.DB_USER);
 console.log("server  =" + process.env.DB_SERVER);
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-    }
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
 });
-server.listen(3005);
-console.log('Socket server listening on port 3005');
+server.listen(SOCKET_PORT);
+console.log("Socket server listening on port " + SOCKET_PORT);
 //server.listen(5012);
 io.on("connection", (client) => {
   console.log("A client connected");
@@ -133,11 +132,10 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(cookieParser());
-
-app.use(express.static(__dirname + '/public'));
-app.set('views', './views');
-app.set('view engine', 'ejs');
-var bodyParser = require('body-parser');
+app.use(express.static(__dirname + "/public"));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+var bodyParser = require("body-parser");
 const { Socket } = require("socket.io");
 const { existsSync } = require("fs");
 app.use(bodyParser.json());
@@ -146,8 +144,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", function (req, res, next) {  
   api_module.checklogin_index(req, res, next);
 });
-app.use('/upload', function (req, res, next) {    
-    api_module.checklogin_index(req, res, next);
+app.use("/upload", function (req, res, next) {
+  api_module.checklogin_index(req, res, next);
 });
 app.use("/uploadfile", function (req, res, next) {
   api_module.checklogin_index(req, res, next);
@@ -155,8 +153,8 @@ app.use("/uploadfile", function (req, res, next) {
 app.use("/login", function (req, res, next) {
   api_module.checklogin_login(req, res, next);
 });
-app.use('/login2', function (req, res, next) {
-    api_module.checklogin_login(req, res, next);
+app.use("/login2", function (req, res, next) {
+  api_module.checklogin_login(req, res, next);
 });
 app.post("/api", function (req, res) {
   //api_module.process_api(req,res);
@@ -173,160 +171,107 @@ app.post("/api", function (req, res) {
     res.send({ tk_status: "ng" });
   }
 });
-
-app.post('/upload',  upload.single('banve'),function (req, res) {    
-    console.log(req.body.filename);
-    if(req.coloiko ==='coloi')    
-    {        
-        if(req.file)
-        {
-            fs.rm('uploadfiles\\' + req.file.originalname), ()=> {
-                console.log("DELETED " + req.file.originalname);
-            };        
-            console.log('successfully deleted ' + 'uploadfiles\\' + req.file.originalname);
-            res.send({tk_status:"NG",message:"Chưa đăng nhập"});
-        }
-        else
-        {
-            res.send({tk_status:"NG",message:"File chưa lên"}); 
-        }
-       
+app.post("/upload", upload.single("banve"), function (req, res) {
+  console.log(req.body.filename);
+  if (req.coloiko === "coloi") {
+    if (req.file) {
+      fs.rm("uploadfiles\\" + req.file.originalname),
+        () => {
+          console.log("DELETED " + req.file.originalname);
+        };
+      console.log(
+        "successfully deleted " + "uploadfiles\\" + req.file.originalname
+      );
+      res.send({ tk_status: "NG", message: "Chưa đăng nhập" });
+    } else {
+      res.send({ tk_status: "NG", message: "File chưa lên" });
     }
-    else if(req.coloiko ==='kocoloi')    
-    {        
-        if(req.file)
-        {
-            const filename = req.file.originalname;
-            const newfilename = req.body.filename;
-            const draw_folder = 'D:\\xampp\\htdocs\\banve\\';
-            console.log('ket qua:' + existsSync(draw_folder + filename));
-
-            if(!existsSync(draw_folder + filename))
-            {              
-                    fs.copyFile('uploadfiles\\' + filename, draw_folder + newfilename +'.pdf', (err) => {
-                    if (err) {
-                        res.send({tk_status:"NG",message:"Upload file thất bại: " + err});
-                    }
-                    else {
-                        fs.rm('uploadfiles\\' + req.file.originalname,(error) => {
-                            //you can handle the error here
-                        }); 
-                        res.send({tk_status:"OK",message:"Upload file thành công"});
-                    }
-                  });
+  } else if (req.coloiko === "kocoloi") {
+    if (req.file) {
+      const filename = req.file.originalname;
+      const newfilename = req.body.filename;
+      const draw_folder = DRAW_PATH;
+      console.log("ket qua:" + existsSync(draw_folder + filename));
+      if (!existsSync(draw_folder + filename)) {
+        fs.copyFile(
+          "uploadfiles\\" + filename,
+          draw_folder + newfilename + ".pdf",
+          (err) => {
+            if (err) {
+              res.send({
+                tk_status: "NG",
+                message: "Upload file thất bại: " + err,
+              });
+            } else {
+              fs.rm("uploadfiles\\" + req.file.originalname, (error) => {
+                //you can handle the error here
+              });
+              res.send({ tk_status: "OK", message: "Upload file thành công" });
             }
-            else
-            {
-                fs.rm('uploadfiles\\' + req.file.originalname,(error) => {
-                    //you can handle the error here
-                }); 
-                console.log('DELETED: '+'uploadfiles\\' + req.file.originalname );
-                res.send({tk_status:"NG",message:"File đã tồn tại"});            
-            }
-        }
-        else
-        {
-            res.send({tk_status:"NG",message:"File chưa lên"});           
-
-        }
-        
-    }   
-});
-
-app.post('/uploadavatar',  upload.single('avatar'),function (req, res) {    
-    console.log(req.body.filename);
-    if(req.coloiko ==='coloi')    
-    {        
-        if(req.file)
-        {
-            fs.rm('uploadfiles\\' + req.file.originalname), ()=> {
-                console.log("DELETED " + req.file.originalname);
-            };        
-            console.log('successfully deleted ' + 'uploadfiles\\' + req.file.originalname);
-            res.send({tk_status:"NG",message:"Chưa đăng nhập"});
-        }
-        else
-        {
-            res.send({tk_status:"NG",message:"File chưa lên"}); 
-        }
-       
-    }
-    else if(req.coloiko ==='kocoloi')    
-    {        
-        if(req.file)
-        {
-            const filename = req.file.originalname;
-            const newfilename = req.body.filename;
-            const draw_folder = 'D:\\xampp\\htdocs\\Picture_NS\\';
-            console.log('ket qua:' + existsSync(draw_folder + filename));
-
-            if(!existsSync(draw_folder + filename))
-            {              
-                    fs.copyFile('uploadfiles\\' + filename, draw_folder + newfilename +'.jpg', (err) => {
-                    if (err) {
-                        res.send({tk_status:"NG",message:"Upload file thất bại: " + err});
-                    }
-                    else {
-                        fs.rm('uploadfiles\\' + req.file.originalname,(error) => {
-                            //you can handle the error here
-                        }); 
-                        res.send({tk_status:"OK",message:"Upload file thành công"});
-                    }
-                  });
-            }
-            else
-            {
-                fs.rm('uploadfiles\\' + req.file.originalname,(error) => {
-                    //you can handle the error here
-                }); 
-                console.log('DELETED: '+'uploadfiles\\' + req.file.originalname );
-                res.send({tk_status:"NG",message:"File đã tồn tại"});            
-            }
-        }
-        else
-        {
-            res.send({tk_status:"NG",message:"File chưa lên"});           
-
-        }
-        
-    }   
-});
-
-
-app.post('/api2', function (req, res) {
-    api_module.process_api(req,res);   
-    var qr = req.body;  
-});
-
-app.get('/', function (req, res) {    
-    //res.redirect('/nhansu');
-    res.redirect('http://14.160.33.94:3010');
-    //res.redirect("14.160.33.94:3010");
-});
-app.get('/nhansu', function (req, res) {
-    res.redirect('http://14.160.33.94:3010');
-    /* if (req.coloiko == 'kocoloi') {
-        res.render('pages/index.ejs',
-            {
-                variable: 'Đây là nội dung của biến được truyền vào', selection: 'result_table', rootPath: __dirname, login_data: req.payload_data
-            }
+          }
         );
+      } else {
+        fs.rm("uploadfiles\\" + req.file.originalname, (error) => {
+          //you can handle the error here
+        });
+        console.log("DELETED: " + "uploadfiles\\" + req.file.originalname);
+        res.send({ tk_status: "NG", message: "File đã tồn tại" });
+      }
+    } else {
+      res.send({ tk_status: "NG", message: "File chưa lên" });
     }
-    else {
-        res.redirect('/login2');
-    } */
+  }
 });
-app.get('/about', function (req, res) {
-    if (req.coloiko == 'kocoloi') {
-        res.render('pages/about.ejs',
-            {
-                variable: 'Đây là nội dung của biến được truyền vào', selection: 'result_table', rootPath: __dirname, login_data: req.payload_data
+app.post("/uploadavatar", upload.single("avatar"), function (req, res) {
+  console.log(req.body.filename);
+  if (req.coloiko === "coloi") {
+    if (req.file) {
+      fs.rm("uploadfiles\\" + req.file.originalname),
+        () => {
+          console.log("DELETED " + req.file.originalname);
+        };
+      console.log(
+        "successfully deleted " + "uploadfiles\\" + req.file.originalname
+      );
+      res.send({ tk_status: "NG", message: "Chưa đăng nhập" });
+    } else {
+      res.send({ tk_status: "NG", message: "File chưa lên" });
+    }
+  } else if (req.coloiko === "kocoloi") {
+    if (req.file) {
+      const filename = req.file.originalname;
+      const newfilename = req.body.filename;
+      const draw_folder = EMPL_IMAGE_PATH;
+      console.log("ket qua:" + existsSync(draw_folder + filename));
+      if (!existsSync(draw_folder + filename)) {
+        fs.copyFile(
+          "uploadfiles\\" + filename,
+          draw_folder + newfilename + ".jpg",
+          (err) => {
+            if (err) {
+              res.send({
+                tk_status: "NG",
+                message: "Upload file thất bại: " + err,
+              });
+            } else {
+              fs.rm("uploadfiles\\" + req.file.originalname, (error) => {
+                //you can handle the error here
+              });
+              res.send({ tk_status: "OK", message: "Upload file thành công" });
             }
+          }
         );
+      } else {
+        fs.rm("uploadfiles\\" + req.file.originalname, (error) => {
+          //you can handle the error here
+        });
+        console.log("DELETED: " + "uploadfiles\\" + req.file.originalname);
+        res.send({ tk_status: "NG", message: "File đã tồn tại" });
+      }
+    } else {
+      res.send({ tk_status: "NG", message: "File chưa lên" });
     }
-    else {
-        res.redirect('/login2');
-    }
+  }
 });
 app.post("/uploadfile", upload2.single("uploadedfile"), function (req, res) {
   console.log("vao uploaded file thanh cong");
