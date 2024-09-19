efa903cd10d5e9d6500dc1e893b2b69ec800c83a
vip7009pro <hung1893@gmail.com>
2024-09-19 08:24:23 +0700

    update api

--- checkLink.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-let url = 'https://drive.google.com/uc?export=download&id=10dJDjoOhkW9aFkim2f0-TeUTLiptcA7i';
-let data = axios.get(url).then((response)=>{
-    console.log(response.data);
-}).catch((error)=> {
-    console.log(error);
-});
-
