d2789f3df9884ae9a6ec263ad4a39b92de2d8bba
vip7009pro <hung1893@gmail.com>
2025-03-15 13:47:28 +0700

    update socket

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -3,58 +3,59 @@ const { Server } = require("socket.io");
 module.exports = (httpServer, httpsServer) => {
   const io = new Server(httpServer, { cors: { origin: "*" } });
   const ios = new Server(httpsServer, { cors: { origin: "*" } });
+  let client_array = [];
 
-  const handleConnection = (socket, ioInstance) => {
+  const handleConnection = (client, ioInstance) => {
     console.log("A client connected");
-    console.log("IO: Connected clients: " + io.engine.clientsCount);
+    console.log("IO: Connected clients: " + ioInstance.engine.clientsCount);
     client.on("send", (data) => {
-      io.sockets.emit("send", data);
+      ioInstance.sockets.emit("send", data);
     });
     client.on("changeServer", (data) => {
-      io.sockets.emit("changeServer", data);
+      ioInstance.sockets.emit("changeServer", data);
     });
-    io.sockets.emit("request_check_online2", { check: 'online' });
-    io.sockets.emit("online_list", client_array);
+    ioInstance.sockets.emit("request_check_online2", { check: 'online' });
+    ioInstance.sockets.emit("online_list", client_array);
     client.on("respond_check_online", (data) => {
       //console.log('co responde check online',data.EMPL_NO)
       if (client_array.filter(item => item.EMPL_NO === data.EMPL_NO).length === 0) client_array.push(data);
     });
     client.on("notification", (data) => {
-      io.sockets.emit("notification", data);
+      ioInstance.sockets.emit("notification", data);
       console.log(data);
     });
     client.on("notification_panel", (data) => {
-      io.sockets.emit("notification_panel", data);
+      ioInstance.sockets.emit("notification_panel", data);
       console.log(data);    
     })
     client.on("online_list", (data) => {
       console.log(data);
     });
     client.on("setWebVer", (data) => {
-      io.sockets.emit("setWebVer", data);
+      ioInstance.sockets.emit("setWebVer", data);
       console.log(data);
     });
     client.on("login", (data) => {
       if (client_array.filter(item => item.EMPL_NO === data.EMPL_NO).length === 0) client_array.push(data);
-      io.sockets.emit("online_list", client_array);
-      io.sockets.emit("login", data + "da dang nhap");
+      ioInstance.sockets.emit("online_list", client_array);
+      ioInstance.sockets.emit("login", data + "da dang nhap");
       console.log(data + " da dang nhap");
     });
     client.on("logout", (data) => {
       client_array = client_array.filter(obj => obj.EMPL_NO !== data.EMPL_NO);
       console.log('client_array', client_array.map((e, i) => e.EMPL_NO));
-      io.sockets.emit("online_list", client_array);
+      ioInstance.sockets.emit("online_list", client_array);
       console.log(data + " da dang xuat");
     });
     client.on("disconnect", (data) => {
       console.log(data);
       console.log("A client disconnected !");
       console.log("Connected clients: " + io.engine.clientsCount);
-      io.sockets.emit("request_check_online2", { check: 'online' });
-      io.sockets.emit("online_list", client_array);
+      ioInstance.sockets.emit("request_check_online2", { check: 'online' });
+      ioInstance.sockets.emit("online_list", client_array);
     });
   };
 
-  io.on("connection", (socket) => handleConnection(socket, io));
-  ios.on("connection", (socket) => handleConnection(socket, ios));
+  io.on("connection", (client) => handleConnection(client, io));
+  ios.on("connection", (client) => handleConnection(client, ios));
 };
