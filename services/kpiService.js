755ff8b6d9d988793dcfd55891b554841cf6d928
vip7009pro <hung1893@gmail.com>
2025-06-26 16:51:39 +0700

    update kpi service

--- services/kpiService.js
+++ services/kpiService.js
@@ -2,8 +2,16 @@ const { queryDB_New } = require("../config/database");
 const moment = require("moment");
 
 exports.loadKPI = async (req, res, DATA) => {
-  let query = `SELECT * FROM ZTB_KPI_TB WHERE CTR_CD = @CTR_CD AND KPI_NAME= @KPI_NAME`;
-  let params = { CTR_CD: DATA.CTR_CD, KPI_NAME: DATA.KPI_NAME};
+  let condition = "";
+  if (DATA.KPI_NAME !== "ALL") condition += ` AND KPI_NAME = '${DATA.KPI_NAME}'`;
+  let query = `SELECT * FROM ZTB_KPI_TB WHERE CTR_CD = @CTR_CD ${condition}`;
+  let params = { CTR_CD: DATA.CTR_CD};
+  let kqua = await queryDB_New(query, params);
+  res.send(kqua);
+};
+exports.loadKPIList = async (req, res, DATA) => {
+  let query = `SELECT DISTINCT KPI_NAME FROM ZTB_KPI_TB WHERE CTR_CD = @CTR_CD`;
+  let params = { CTR_CD: DATA.CTR_CD};
   let kqua = await queryDB_New(query, params);
   res.send(kqua);
 };
