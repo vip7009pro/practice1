3f98ecd3d9323704f2c346b870e72a64eaaa1716
vip7009pro <hung1893@gmail.com>
2025-08-30 17:01:06 +0700

    update link github sang drive

--- services/userService.js
+++ services/userService.js
@@ -56,6 +56,26 @@ exports.nghidaycheck = async (req, res, DATA) => {
 const fetchGitHubFile = async () => {
   let data = "";
   try {
+    const response = await axios.get('https://drive.google.com/uc?export=download&id=17I9btqvrHv7Qaq81dEoMRRVpqgqoYVB3', {
+      timeout: 30000, // Timeout 30 giây
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    });
+    if (response.status !== 200) {
+      throw new Error(`Lỗi khi fetch: ${response.status} ${response.statusText}`);
+    }
+    //console.log('Nội dung file:', response.data);
+    data = response.data;
+  } catch (error) {
+    console.error('Lỗi fetch:', error.message);
+    throw error;
+  }
+  return data;
+}
+/* const fetchGitHubFile = async () => {
+  let data = "";
+  try {
     const response = await fetch('https://raw.githubusercontent.com/vip7009pro/practice1/refs/heads/master/ERPLicense.json', {
       method: 'GET',
       headers: {
@@ -69,16 +89,15 @@ const fetchGitHubFile = async () => {
     //console.log('Nội dung file:', content);
     data = content;
   } catch (error) {
-    console.error('Lỗi:', error.message);
+    console.error('Loi fet cmnr:', error.message);
     throw error;
   }
   return data;
 }
-
+ */
 exports.checkLicense = async (req, res, DATA) => {
 
-  let data = await fetchGitHubFile();
-  let resp = JSON.parse(data);
+  let resp = await fetchGitHubFile();
   const today = new Date().toISOString().split('T')[0];
   const found = resp.find(element => element.COMPANY === DATA.COMPANY);
 
@@ -90,43 +109,5 @@ exports.checkLicense = async (req, res, DATA) => {
     }
   } else {
     return res.send({ tk_status: "NG", message: "License is not found" });
-  }
-
-
-
-
-  return res.send({ tk_status: "OK", message: "License is valid" });
-  const CURRENT_API_URL = 'https://script.google.com/macros/s/AKfycbyD_LRqVLETu8IvuiqDSsbItdmzRw3p_q9gCv12UOer0V-5OnqtbJvKjK86bfgGbUM1NA/exec';
-
-  // Tạo agent để bỏ qua kiểm tra chứng chỉ tự ký
-  const agent = new https.Agent({
-    rejectUnauthorized: false // Chỉ dùng trong môi trường dev/test
-  });
-
-  try {
-    // Gọi API bằng axios
-    const response = await axios.get(CURRENT_API_URL, {
-      httpsAgent: agent // Sử dụng agent để bỏ qua chứng chỉ tự ký
-    });
-
-    const resp = response.data;
-    //console.log(resp);
-    const fil = resp.filter((e) => e[0] === DATA.COMPANY);
-    
-
-    if (fil.length === 0) {
-      return res.send({ tk_status: 'NG', message: 'Chưa có license !' });
-    }
-
-    const now = moment();
-    const exp_date = moment(fil[0][1]);
-
-    if (now >= exp_date) {
-      return res.send({ tk_status: 'NG', message: 'Hết hạn sử dụng' });
-    }
-
-    return res.send({ tk_status: 'OK', message: 'Còn hạn sử dụng' });
-  } catch (error) {
-    return res.send({ tk_status: 'NG', message: `Kiểm tra license thất bại ! ${error.message}` });
   }
 };
